ddd3f298104d8a454239a35013f55ccf9e836ff0
Ismail Aidar <ismailaidar@gmail.com>
2024-03-15 09:13:34 -0400

    add validations to projet model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,20 @@
 class Project < ApplicationRecord
+  # Associations
   belongs_to :user
+
+  # Valid statuses for projects
+  enum status: { active: 'active', completed: 'completed', on_hold: 'on_hold' }
+
+  # Validations
+  validates :name, presence: true
+  validates :status, presence: true, inclusion: { in: statuses.keys }
+  validates :end_date, presence: true, comparison: { greater_than: :start_date }
+
+  after_initialize :set_default_status, if: :new_record?
+
+  private
+
+  def set_default_status
+    self.status ||= :active
+  end
 end
