ae442f7b0e1acf61c1ee4fb95c2472209990618e
Ismail Aidar <ismailaidar@gmail.com>
2024-03-15 09:14:01 -0400

    add default status to task table

--- db/migrate/20240315130333_create_tasks.rb
+++ db/migrate/20240315130333_create_tasks.rb
@@ -4,7 +4,7 @@ class CreateTasks < ActiveRecord::Migration[7.1]
       t.string :title, null: false
       t.string :description
       t.date :due_date
-      t.string :status
+      t.string :status, default: 'active', null: false
       t.integer :priority
       t.references :project, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
