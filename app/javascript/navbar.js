d5ad2951ab131756defe881ef346b9903a7fae1d
Ismail Aidar <ismailaidar@gmail.com>
2024-03-18 09:34:28 -0400

    create js file for the navbar menu

---
+++ app/javascript/navbar.js
@@ -0,0 +1,28 @@
+document.addEventListener("turbo:load", function () {
+  document.getElementById('user-menu-button').addEventListener('click', function () {
+    var dropdownMenu = document.querySelector('[aria-labelledby="user-menu-button"]');
+    if (dropdownMenu.classList.contains('hidden')) {
+      dropdownMenu.classList.remove('hidden');
+      dropdownMenu.classList.add('block');
+    } else {
+      dropdownMenu.classList.add('hidden');
+      dropdownMenu.classList.remove('block');
+    }
+  });
+})
+
+document.addEventListener('DOMContentLoaded', function () {
+  // Find the button and the menu element
+  const toggleButton = document.querySelector('button[aria-controls="mobile-menu"]');
+  const menu = document.getElementById('mobile-menu');
+
+  // Add a click event listener to the button
+  toggleButton.addEventListener('click', function () {
+    // Toggle the 'hidden' class on the menu
+    menu.classList.toggle('hidden');
+
+    // Update aria-expanded attribute based on visibility
+    const isExpanded = toggleButton.getAttribute('aria-expanded') === 'true';
+    toggleButton.setAttribute('aria-expanded', !isExpanded);
+  });
+});
