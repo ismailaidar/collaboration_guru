1380007f6f8a6f6213e177b1e5a387bbe270dde4
Ismail Aidar <ismailaidar@gmail.com>
2024-03-20 11:25:29 -0400

    fetch users using js, and add them to select

--- app/javascript/controllers/search_user_controller.js
+++ app/javascript/controllers/search_user_controller.js
@@ -1,7 +1,34 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  connect() {
-    console.log('connected')
+  static targets = ["userSelect", "emailInput"]
+
+  query() {
+    const select = this.userSelectTarget;
+    
+    // clear the select first
+    this.reset(select)
+
+    // add more options based on the search
+    const url = `/users/fetch_users?userInput=${this.emailInputTarget.value}`
+    fetch(url, {
+      headers: {
+        Accept: "application/json"
+      }
+    })
+    .then(response => response.json())
+    .then(response => {
+      const option = document.createElement('option');
+      response.forEach(element => {
+        option.text = element.email
+        select.add(option)
+      });
+    })
+  }
+
+  reset(selectElement) {
+    while (selectElement.options.length > 0) {
+      selectElement.remove(0);
+    }
   }
 }
