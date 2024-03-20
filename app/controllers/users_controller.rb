7ebddc30303876504933956986bfae13f808da1c
Ismail Aidar <ismailaidar@gmail.com>
2024-03-20 11:24:50 -0400

    create fetch method to fetch users based on query param

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+  def fetch_users
+    users = if params[:userInput].present?
+              User.where('email LIKE ?', "#{params[:userInput]}%").limit(5)
+            else
+              User.none
+            end
+    render json: users
+  end
 end
