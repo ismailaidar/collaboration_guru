23311342b85ca33fadbae6031b283c7017931e49
Ismail Aidar <ismailaidar@gmail.com>
2024-03-20 11:23:49 -0400

    create users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   resources :projects
+  get 'users/fetch_users', to: 'users#fetch_users'
   get 'pages/index'
   devise_for :users
   get 'up' => 'rails/health#show', as: :rails_health_check
   root 'pages#index'
+
+  get 'users/search', to: 'users#search'
 end
