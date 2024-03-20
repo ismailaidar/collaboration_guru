4b025fb4608e28272123646377200328bc5933b9
Ismail Aidar <ismailaidar@gmail.com>
2024-03-20 14:00:14 -0400

    add tags when selecting an email

--- app/javascript/controllers/search_user_controller.js
+++ app/javascript/controllers/search_user_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["userSelect", "emailInput"]
+  static targets = ["userSelect", "emailInput", "emailTags"]
   
   connect() {
     this.query = this.debounce(this.query, 900); 
@@ -25,7 +25,7 @@ export default class extends Controller {
       response.forEach(element => {
         const option = document.createElement('option'); // Moved inside loop
         option.text = element.email;
-        option.value = element.email; // Assuming you'd also want to set the value
+        option.value = element.id;
         select.add(option);
       });
     })
@@ -35,6 +35,35 @@ export default class extends Controller {
     while (selectElement.options.length > 0) {
       selectElement.remove(0);
     }
+  }
+
+  addEmailTag() {
+    // Get only the first selection since I am only firing this action on click
+    // so I don't need all the selected values but only the first one
+    // as soon as it's click I add a tag to the list.
+    const option = this.userSelectTarget.options[0];
+    if (option) {
+      const tag = this.createTag(option);
+      this.emailTagsTarget.appendChild(tag);
+    }
+  }
+
+  createTag(option) {
+    const wrapper = document.createElement('div');
+    wrapper.innerHTML = `
+      <span class="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10" id="${option.value}">
+        ${option.text}
+        <button class="ml-2 inline-flex items-center justify-center rounded-full bg-red-300 p-1 text-white hover:bg-red-500 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 text-xs" 
+                type="button" data-action="click->search-user#removeTag">
+          X
+        </button>
+      </span>
+    `;
+    return wrapper.firstElementChild;
+  }
+
+  removeTag(event) {
+    event.target.closest('span').remove();
   }
 
   debounce(func, wait) {
